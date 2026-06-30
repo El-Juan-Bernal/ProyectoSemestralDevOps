6ddfeae593c1defcc3c4dba94d277165e664fdcd
El-Juan-Bernal <ju.bernala@duocuc.cl>
2026-06-29 22:28:13 -0400

    Sincroniza nombres de repositorios con AWS ECR

--- front_despacho/vite.config.js
+++ front_despacho/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -14,3 +15,9 @@ export default defineConfig({
     }
   }
 })
+
+
+
+
+
+
